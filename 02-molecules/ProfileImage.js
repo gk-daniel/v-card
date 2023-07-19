aaaf9f80a626060935d6d7af4a513ed2890eae3b
Daniel G. Kostyushko <daniil.grudokkostyushko@gastro-hero.de>
2023-07-19 18:18:39 +0200

    fix: correct url of profile image in ProfileImage.js

--- 02-molecules/ProfileImage.js
+++ 02-molecules/ProfileImage.js
@@ -4,7 +4,7 @@
 app.component('profile-image', {
   template: `
     <div class="profile-image">
-      <images :src="'https://dn.gooko.org/projects/vue/card/00-config/assets/images/daniel-g-kostyushko.jpg'" />
+      <images :src="'https://dn.gooko.org/vue/card/00-config/assets/images/daniel-g-kostyushko.jpg'" />
     </div>
   `,
 });
