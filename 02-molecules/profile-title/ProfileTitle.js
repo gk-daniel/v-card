4fe5a3a55e442e39baa7265ea3a188617212a80e
Daniel G. Kostyushko <daniil.grudokkostyushko@gastro-hero.de>
2023-07-25 20:21:10 +0200

    style: add formating style

--- 02-molecules/profile-title/ProfileTitle.js
+++ 02-molecules/profile-title/ProfileTitle.js
@@ -1,10 +1,10 @@
-/* 
-* ============================================== 
-* 02-molecules/profile-title/ProfileTitle.js
-* ============================================== 
+/*
+  * ==============================================
+  * 02-molecules/profile-title/ProfileTitle.js
+  * ==============================================
 */
 
-app.component('profile-title', {
+app.component("profile-title", {
   template: `
   <h2 class="profile-username">Daniel G. Kostyushko</h2>
   <h3 class="profile-user-handle">senior software engineer</h3>
