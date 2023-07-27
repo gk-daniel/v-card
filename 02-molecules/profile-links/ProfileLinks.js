b59d9c4e8fc17d4d5ba3cf1065f295c8af33a8b5
Daniel G. Kostyushko <daniil.grudokkostyushko@gastro-hero.de>
2023-07-27 19:21:48 +0200

    refactor: add classical structure of component

--- 02-molecules/profile-links/ProfileLinks.js
+++ 02-molecules/profile-links/ProfileLinks.js
@@ -9,10 +9,10 @@
 app.component("ProfileLinks", {
   template: `
     <div class="profile-links">
-    <links :href="'https://www.linkedin.com/in/daniel-g-kostyushko/'" :icons="'ph-linkedin-logo'"/>
-    <links :href="'https://www.xing.com/profile/Daniel_GKostyushko/'" :icons="'ph-chat-teardrop-text'"/>
-    <links :href="'https://www.instagram.com/gk.daniel/'" :icons="'ph-instagram-logo'"/>
-    <links :href="'https://www.facebook.com/daniel.g.kostyushko'" :icons="'ph-facebook-logo'"/>
+      <links :href="'https://www.linkedin.com/in/daniel-g-kostyushko/'" :icons="'ph-linkedin-logo'"/>
+      <links :href="'https://www.xing.com/profile/Daniel_GKostyushko/'" :icons="'ph-chat-teardrop-text'"/>
+      <links :href="'https://www.instagram.com/gk.daniel/'" :icons="'ph-instagram-logo'"/>
+      <links :href="'https://www.facebook.com/daniel.g.kostyushko'" :icons="'ph-facebook-logo'"/>
     </div>
   `,
 });
